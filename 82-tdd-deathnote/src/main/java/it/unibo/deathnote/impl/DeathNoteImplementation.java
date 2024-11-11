33d367a5509a0e532a13d7457821b25aef72b758
Matteo Grandini <matteo.grandini@studio.unibo.it>
2024-11-11 12:58:48 +0100

    first test impl and passed

---
+++ 82-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImplementation.java
@@ -0,0 +1,52 @@
+package it.unibo.deathnote.impl;
+
+import it.unibo.deathnote.api.DeathNote;
+
+public class DeathNoteImplementation implements DeathNote {
+
+    @Override
+    public String getRule(int ruleNumber) {
+        if (ruleNumber < 1 || ruleNumber > DeathNote.RULES.size()) {
+            throw new IllegalArgumentException("Rule number must be between 1 and 13");
+        }
+        return DeathNote.RULES.get(ruleNumber - 1);
+    }
+
+    @Override
+    public void writeName(String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'writeName'");
+    }
+
+    @Override
+    public boolean writeDeathCause(String cause) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'writeDeathCause'");
+    }
+
+    @Override
+    public boolean writeDetails(String details) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'writeDetails'");
+    }
+
+    @Override
+    public String getDeathCause(String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDeathCause'");
+    }
+
+    @Override
+    public String getDeathDetails(String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDeathDetails'");
+    }
+
+    @Override
+    public boolean isNameWritten(String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isNameWritten'");
+    }
+
+    
+}
--- 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,5 +1,53 @@
 package it.unibo.deathnote;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.function.Executable;
+import org.junit.platform.commons.util.StringUtils;
+
+import it.unibo.deathnote.impl.DeathNoteImplementation;
+import it.unibo.deathnote.api.DeathNote;
+
+import org.junit.jupiter.api.Test;
+
 class TestDeathNote {
 
+    private final static int NUM_OF_RULES = 13;
+
+    @Test
+    void testGetRule() {
+
+        final DeathNote dn = new DeathNoteImplementation();
+
+        Exception e = assertThrowsExactly(IllegalArgumentException.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                dn.getRule(0);
+            }
+        });
+        assertNotNull(e.getMessage());
+        assertTrue(StringUtils.isNotBlank(e.getMessage()));
+
+        e = assertThrowsExactly(IllegalArgumentException.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                dn.getRule(-1);
+            }
+        });
+        assertNotNull(e.getMessage());
+        assertTrue(StringUtils.isNotBlank(e.getMessage()));
+
+        e = assertThrowsExactly(IllegalArgumentException.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                dn.getRule(NUM_OF_RULES + 1);
+            }
+        });
+        assertNotNull(e.getMessage());
+        assertTrue(StringUtils.isNotBlank(e.getMessage()));
+
+        for (int i = 1; i <= NUM_OF_RULES; i++) {
+            assertNotNull(dn.getRule(i));
+            assertTrue(StringUtils.isNotBlank(dn.getRule(i)));
+        }
+    }
 }
