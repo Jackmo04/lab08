63bf9c7a335b0f6bbc26d7d20d779cf77c40b02e
Matteo Grandini <matteo.grandini@studio.unibo.it>
2024-11-17 18:11:24 +0100

    changed list from Linked to Array

--- 82-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImplementation.java
+++ 82-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImplementation.java
@@ -1,6 +1,6 @@
 package it.unibo.deathnote.impl;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -14,7 +14,7 @@ public class DeathNoteImplementation implements DeathNote {
     private long lastWritingTimeMillis;
 
     public DeathNoteImplementation() {
-        this.entries = new LinkedList<>();
+        this.entries = new ArrayList<>();
     }
 
     @Override
